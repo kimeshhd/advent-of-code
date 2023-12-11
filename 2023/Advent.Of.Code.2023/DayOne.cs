3e4855ffa00254e727ccd9c9beabd088535e13cf
Kimeshan Naidoo <kimeshhd@gmail.com>
2023-12-11 13:47:48 +0200

    add day two part 1

---
+++ 2023/Advent.Of.Code.2023/DayOne.cs
@@ -0,0 +1,155 @@
+﻿namespace Advent.Of.Code._2023
+{
+    public static class DayOne
+    {
+        public static void Solve()
+        {
+            var lines = File.ReadAllLines("puzzle-1.txt");
+            var numbers = new List<int>();
+
+            changeToNumbers(lines);
+
+            foreach (var line in lines)
+            {
+                int firstDigit = 0;
+                int lastDigit = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (int.TryParse(line[i].ToString(), out firstDigit))
+                        break;
+                }
+
+                for (int i = line.Length - 1; i >= 0; i--)
+                {
+                    if (int.TryParse(line[i].ToString(), out lastDigit))
+                        break;
+                }
+
+                var num = firstDigit + "" + lastDigit;
+
+                numbers.Add(int.Parse(num));
+            }
+
+            int total = 0;
+
+            foreach (var number in numbers)
+            {
+                total += number;
+            }
+
+            Console.Out.WriteLine(total.ToString());
+
+            void changeToNumbers(string[] lines)
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    do
+                    {
+                        lines[i] = transformLine(lines[i]);
+                    } while (containsWordNumbers(lines[i]));
+                }
+            }
+
+            bool containsWordNumbers(string line)
+            {
+                if (line.Contains("one"))
+                {
+                    return true;
+                }
+
+                if (line.Contains("two"))
+                {
+                    return true;
+                }
+
+                if (line.Contains("three"))
+                {
+                    return true;
+                }
+
+                if (line.Contains("four"))
+                {
+                    return true;
+                }
+
+                if (line.Contains("five"))
+                {
+                    return true;
+                }
+
+                if (line.Contains("six"))
+                {
+                    return true;
+                }
+
+                if (line.Contains("seven"))
+                {
+                    return true;
+                }
+
+                if (line.Contains("eight"))
+                {
+                    return true;
+                }
+
+                if (line.Contains("nine"))
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            string transformLine(string line)
+            {
+                if (line.Contains("one"))
+                {
+                    line = line.Replace("one", "o1e");
+                }
+
+                if (line.Contains("two"))
+                {
+                    line = line.Replace("two", "t2o");
+                }
+
+                if (line.Contains("three"))
+                {
+                    line = line.Replace("three", "t3e");
+                }
+
+                if (line.Contains("four"))
+                {
+                    line = line.Replace("four", "f4r");
+                }
+
+                if (line.Contains("five"))
+                {
+                    line = line.Replace("five", "f5e");
+                }
+
+                if (line.Contains("six"))
+                {
+                    line = line.Replace("six", "s6x");
+                }
+
+                if (line.Contains("seven"))
+                {
+                    line = line.Replace("seven", "s7n");
+                }
+
+                if (line.Contains("eight"))
+                {
+                    line = line.Replace("eight", "e8t");
+                }
+
+                if (line.Contains("nine"))
+                {
+                    line = line.Replace("nine", "n9e");
+                }
+
+                return line;
+            }
+        }
+    }
+}
---
+++ 2023/Advent.Of.Code.2023/DayTwo.cs
@@ -0,0 +1,91 @@
+﻿namespace Advent.Of.Code._2023
+{
+    public static class DayTwo
+    {
+        public static void Solve()
+        {
+            var lines = File.ReadAllLines("day-2.txt");
+
+            var totalRedCubes = 12;
+            var totalGreenCubes = 13;
+            var totalBlueCubes = 14;
+
+            var validGames = new List<int>();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string? line = lines[i];
+
+                var validRound = false;
+
+                var gameId = int.Parse(line.Substring(0, line.IndexOf(":")).Replace("Game", string.Empty));
+
+                line = line.Replace($"Game {gameId}:", string.Empty);
+
+                var rounds = line.Split(";");
+
+                for (int r = 0; r < rounds.Length; r++)
+                {
+                    var redTakenOut = 0;
+                    var greenTakenOut = 0;
+                    var blueTakenOut = 0;
+
+                    var lowestRed = 100;
+                    var lowestGreen = 100;
+                    var lowestBlue = 100;
+
+                    var cubes = rounds[r].Split(",");
+
+                    for (int c = 0; c < cubes.Length; c++)
+                    {
+                        string? cube = cubes[c];
+                        cubes[c] = cube.Substring(1, cube.Length - 1);
+                    }
+
+                    for (int c = 0; c < cubes.Length; c++)
+                    {
+                        var amountTakenOut = int.Parse(cubes[c].Substring(0, 2));
+
+                        cubes[c] = cubes[c].Replace(amountTakenOut.ToString(), string.Empty);
+
+                        var colour = cubes[c].Trim();
+
+                        switch (colour)
+                        {
+                            case "red":
+                                redTakenOut += amountTakenOut;
+                                break;
+                            case "green":
+                                greenTakenOut += amountTakenOut;
+                                break;
+                            case "blue":
+                                blueTakenOut += amountTakenOut;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    if (totalRedCubes >= redTakenOut && totalGreenCubes >= greenTakenOut && totalBlueCubes >= blueTakenOut)
+                    {
+                        validRound = true;
+                    }
+                    else
+                    {
+                        validRound = false;
+                        break;
+                    }
+                }
+
+                if (validRound)
+                {
+                    validGames.Add(gameId);
+                }
+            }
+
+            Console.WriteLine(validGames.Sum(gameId => gameId));
+
+            Console.Read();
+        }
+    }
+}
